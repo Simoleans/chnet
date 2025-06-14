735620323e932adc94f147549c126b4bde47de10
Fran <francisco20990@gmail.com>
2025-06-14 12:16:23 -0400

    deploy

--- deploy.php
+++ deploy.php
@@ -3,16 +3,19 @@
 echo "🚀 Iniciando deploy...\n";
 
 // Establecer directorio de trabajo
-chdir(__DIR__ . '/app-panel');
+chdir(__DIR__);
 
 // Detectar PHP disponible
 exec('which php', $which);
+exec('which composer', $which);
 $php = $which[0] ?? 'php';
+$composer = $which[1] ?? 'composer';
 echo "PHP encontrado: $php\n";
+echo "Composer encontrado: $composer\n";
 
 // Ejecutar composer install
 echo "Ejecutando composer install...\n";
-exec("export COMPOSER_HOME=/tmp && $php /opt/cpanel/composer/bin/composer install --no-dev --optimize-autoloader 2>&1", $composerOutput);
+exec("export COMPOSER_HOME=/tmp && $composer install --no-dev --optimize-autoloader 2>&1", $composerOutput);
 echo implode("\n", $composerOutput);
 
 // Limpiar cachés
